984898b6176e8110e5b91fb80add46bbbe671037
Ash <ashdevfr@gmail.com>
2015-02-26 20:47:15 +0100

    Add security to avoid nginx crashes

--- nginx-env.sh
+++ nginx-env.sh
@@ -3,6 +3,23 @@
 CONF_DIR=/etc/nginx/conf.d
 FILE_PATTERN=${CONF_DIR}/*.conf
 
+ls ${FILE_PATTERN} | while read file
+do
+        typeset i=$(cat ${file}  | grep -e '\#\#IF\#__|\([a-zA-Z0-9\_\-\$]*\)|__' -o)
+        typeset condition=$(echo "${i}" | wc -w)
+        if [ "${enabled}" != "0" ]
+        then
+                typeset varname=$(echo ${i} | sed -e 's%\#\#IF\#__|\([a-zA-Z0-9\_\-\$]*\)|__%\1%')
+                typeset evalvar=$(eval echo "\$${varname}")
+                typeset enabled=$(echo "${evalvar}" | wc -w)
+                echo "${enabled}"
+                if [ "${enabled}" == "0" ]
+                then
+                        mv ${file} ${file}.bak
+                fi
+        fi
+done
+
 cat /dev/null > sed.script
 cat ${FILE_PATTERN} | grep -e  '__|\([a-zA-Z0-9\_\-\$]*\)|__' -o | while read var
 do
@@ -17,6 +34,5 @@ do
 	# cat ${file}
 done
 rm -f sed.script
-env
 echo "Start Nginx"
 service nginx start
